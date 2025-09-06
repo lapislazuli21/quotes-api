60b7bcb1914772059baaf15ee810e741d07977ed
khushgandhi24 <167192493+khushgandhi24@users.noreply.github.com>
2025-09-06 19:09:40 +0530

    .fix: auth.py error handling logic

--- src/quotes_api/core/auth.py
+++ src/quotes_api/core/auth.py
@@ -1,11 +1,13 @@
+from typing import Optional
+
 from fastapi import Security, HTTPException, status
 from fastapi.security import APIKeyHeader
 
 from .config import get_settings
 
-api_key_header = APIKeyHeader(name="X-API-Key")
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
-def get_api_key(api_key: str = Security(api_key_header)):
+def get_api_key(api_key: Optional[str] = Security(api_key_header)):
     """
     Checks if the provided API key in the 'X-API-Key' header is valid.
     """
